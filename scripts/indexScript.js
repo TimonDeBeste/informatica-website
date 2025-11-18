3478538861623fbfbfd9c6e6f90d1fa8aa4e203a
TimonDeBeste <123480186+TimonDeBeste@users.noreply.github.com>
2025-11-18 12:01:47 +0100

    Initial commit

---
+++ scripts/indexScript.js
@@ -0,0 +1,55 @@
+
+const welcomeMessages = [
+	"Welkom bij de codeclub!",
+	"Dit is de codeclub.",
+	"Word een deel van de codeclub!"
+];
+
+const random = Math.floor(Math.random() * welcomeMessages.length);
+document.getElementById("welkom").textContent = welcomeMessages[random];
+
+function nextFridayAtTwo() {
+	const now = new Date();
+	const result = new Date(now);
+
+	// vrijdag = 5 (maandag=1, zondag=0)
+	const day = result.getDay();
+	const daysUntilFriday = (5 - day + 7) % 7;
+
+	// zet datum op komende vrijdag
+	result.setDate(result.getDate() + daysUntilFriday);
+	result.setHours(14, 0, 0, 0);
+
+	// als het NU al voorbij vrijdag 14:00 is → pak volgende week
+	if (result <= now) {
+		result.setDate(result.getDate() + 7);
+	}
+	console.log(result);
+	return result;
+}
+
+
+
+var countDownDate = nextFridayAtTwo();
+console.log(countDownDate)
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get today's date and time
+var now = new Date().getTime();
+
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
+
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+var result = "nog " + days + " dagen, " + hours + " uur, " + minutes + " minuten en " + seconds + " seconden tot de volgende Codeclub!";
+
+// Output the result in an element with id="countdown"
+document.getElementById("countdown").innerText = result;
+
+}, 1000);
+
